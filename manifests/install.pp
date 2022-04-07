adeb8ddd2e5427bca39479b23150c79f13e14d6c
Ash Crosby <tcrosby@gmail.com>
2022-04-07 16:26:15 +0100

    install node_modules

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,12 +3,23 @@ class nodered::install inherits nodered {
   require nodered::package
 
   $nodered::flows.each |$flow_name, $flow| {
-    vcsrepo { "${nodered::flow_dir}/${flow['path']}":
+    $path = "${nodered::flow_dir}/${flow['path']}"
+
+    vcsrepo { $path:
       ensure   => latest,
       user     => $nodered::owner,
       provider => git,
       source   => $flow['source'],
       identity => "${nodered::real_home}/.ssh/${flow['key']}"
+    }
+
+    nodejs::npm { $path:
+      ensure           => 'present',
+      target           => $path,
+      use_package_json => true,
+      user             => $nodered::owner,
+      home_dir         => "${nodered::real_home}/.npm",
+      require          => Vcsrepo[$path]
     }
   }
 }
